5fbafb92628c31a235f67f0e7ea95a80ff8c57d9
krish <krishnasaibairi56140@gmail.com>
2025-07-03 13:57:55 +0530

    new commit

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Menubar from './components/Menubar/Menubar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Explore from './pages/Explore/Explore';
+import Contact from './pages/Contact/Contact';
 
 const App = () => {
   return (
     <div>
       <Menubar />
+      <Routes>
+        <Route path='/' element = {<Home />}/>
+        <Route path='/explore' element = {<Explore />} />
+        <Route path='/contact' element = {<Contact />} />
+      </Routes>
     </div>
   )
 }
--- foodies/src/assets/assets.js
+++ foodies/src/assets/assets.js
@@ -1,7 +1,70 @@
 import logo from './logo.png';
 import cart from './cart.png';
+import biryani from './biryani.png';
+import burger from './burger.png';
+import cakes from './cakes.png';
+import delivery from './delivery.png';
+import header from './header.png';
+import icecream from './icecream.png';
+import icecream1 from './icecream1.png';
+import login from './login.jpeg';
+import pizza from './pizza.png';
+import profile from './profile.png';
+import rolls from './rolls.png';
+import salad from './salad.png';
 
 export const assets = {
     logo,
     cart
 }
+
+export const categories = [
+    {
+        category : 'biryani',
+        icon : biryani
+    },
+    {
+        category : 'burger',
+        icon : burger
+    },
+    {
+        category : 'cakes',
+        icon : cakes
+    },
+    {
+        category : 'delivery',
+        icon : delivery
+    },
+    {
+        category : 'header',
+        icon : header
+    },
+    {
+        category : 'icecream',
+        icon : icecream
+    },
+    {
+        category : 'icecream1',
+        icon : icecream1
+    },
+    {
+        category : 'login',
+        icon : login
+    },
+    {
+        category : 'pizza',
+        icon : pizza
+    },
+    {
+        category : 'profile',
+        icon : profile
+    },
+    {
+        category : 'rolls',
+        icon : rolls
+    },
+    {
+        category : 'salad',
+        icon : salad
+    },
+]
